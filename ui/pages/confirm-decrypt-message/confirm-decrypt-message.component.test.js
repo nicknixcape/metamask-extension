bdb35bb1629ae87e350ae098ae41039b4bd087cb
Howard Braham <howrad@gmail.com>
2026-09-20 00:32:22 -0400

    test(ui): remove legacy console warnings

--- ui/pages/confirm-decrypt-message/confirm-decrypt-message.component.test.js
+++ ui/pages/confirm-decrypt-message/confirm-decrypt-message.component.test.js
@@ -80,6 +80,16 @@ jest.mock('../../hooks/useScrollRequired', () => ({
   useScrollRequired: jest.fn(),
 }));
 
+jest.mock('../../hooks/useCopyToClipboard', () => ({
+  useCopyToClipboard: () => [
+    false,
+    async (text) => {
+      await globalThis.navigator.clipboard.writeText(text);
+      return true;
+    },
+  ],
+}));
+
 jest.mock('../../store/actions', () => ({
   cancelDecryptMsg: jest.fn(),
   decryptMsg: jest.fn(),
@@ -126,8 +136,10 @@ describe('ConfirmDecryptMessage Component', () => {
     const result = renderWithProvider(<ConfirmDecryptMessage />, store);
 
     const unlockButton = result.getByTestId('message-lock');
-    fireEvent.click(unlockButton);
-    await flushPromises();
+    await act(async () => {
+      fireEvent.click(unlockButton);
+      await flushPromises();
+    });
 
     if (expectError) {
       await waitFor(() => {
@@ -182,8 +194,10 @@ describe('ConfirmDecryptMessage Component', () => {
     const { getByText } = renderWithProvider(<ConfirmDecryptMessage />, store);
 
     const confirmButton = getByText(messages.decrypt.message);
-    fireEvent.click(confirmButton);
-    await flushPromises();
+    await act(async () => {
+      fireEvent.click(confirmButton);
+      await flushPromises();
+    });
 
     expect(mockDecryptMsg).toHaveBeenCalled();
     expect(mockTrackEvent).toHaveBeenCalled();
@@ -193,8 +207,10 @@ describe('ConfirmDecryptMessage Component', () => {
     const { getByText } = renderWithProvider(<ConfirmDecryptMessage />, store);
 
     const confirmButton = getByText(messages.cancel.message);
-    fireEvent.click(confirmButton);
-    await flushPromises();
+    await act(async () => {
+      fireEvent.click(confirmButton);
+      await flushPromises();
+    });
 
     expect(mockCancelDecryptMsg).toHaveBeenCalled();
     expect(mockTrackEvent).toHaveBeenCalled();
@@ -265,7 +281,9 @@ describe('ConfirmDecryptMessage Component', () => {
       const { getByTestId } = await renderAndUnlockMessage();
 
       const scrollToBottomButton = getByTestId('scroll-to-bottom');
-      fireEvent.click(scrollToBottomButton);
+      await act(async () => {
+        fireEvent.click(scrollToBottomButton);
+      });
 
       expect(spyScrollToBottomAction).toHaveBeenCalled();
     });
